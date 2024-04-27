90c9c4b0aad3fd4794adebacb511ddbb9ee8b90e
Hamster-Prime <60058434+Hamster-Prime@users.noreply.github.com>
2024-04-27 18:57:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,21 +2,21 @@
 while true; do
     # 提示用户选择操作
     echo "请选择要执行的操作："
-    echo "1. 安装Clash_TUN"
-    echo "2. 安装Clash_TProxy"
+    echo "1. 安装 OSPF + Clash_TUN"
+    echo "2. 安装 OSPF + Clash_TProxy"
     echo "3. 安装Bird并配置OSPF"
     echo "4. 退出"
     read -p "请输入操作编号： " option
 
     # 根据用户输入执行相应操作
     case "$option" in
-        1)  # 安装Clash_TUN
+        1)  # 安装 OSPF + Clash_TUN
             wget https://raw.githubusercontent.com/Hamster-Prime/ospf-clash/main/install-clash_tun.sh && chmod +x install-clash_tun.sh && ./install-clash_tun.sh
             ;;
-        2)  # 安装Clash_TProxy
+        2)  # 安装 OSPF + Clash_TProxy
             wget https://raw.githubusercontent.com/Hamster-Prime/ospf-clash/main/install-clash_tproxy.sh && chmod +x install-clash_tproxy.sh && ./install-clash_tproxy.sh
             ;;
-        3)  # 安装Bird并配置OSPF
+        3)  # 仅安装OSPF
             wget https://raw.githubusercontent.com/Hamster-Prime/ospf-clash/main/install-ospf.sh && chmod +x install-ospf.sh && ./install-ospf.sh
             ;;
         4)  # 退出
