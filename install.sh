f0e63c24a738125d6c93eb543ff59416b85f9ab6
Hamster-Prime <60058434+Hamster-Prime@users.noreply.github.com>
2024-04-27 18:59:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ while true; do
     echo "请选择要执行的操作："
     echo "1. 安装 OSPF + Clash_TUN"
     echo "2. 安装 OSPF + Clash_TProxy"
-    echo "3. 安装Bird并配置OSPF"
+    echo "3. 仅安装OSPF"
     echo "4. 退出"
     read -p "请输入操作编号： " option
 
