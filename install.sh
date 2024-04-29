8994dd69dc25e110857ade9395ccdd320eb191ed
Hamster-Prime <60058434+Hamster-Prime@users.noreply.github.com>
2024-04-29 08:26:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ while true; do
     echo ""
     echo "2. OSPF + Clash TProxy"
     echo ""
-    echo "3. 仅OSPF"
+    echo "3. EasyMosDNS"
     echo ""
-    echo "4. 退出"
+    echo "4. 仅OSPF"
+    echo ""
+    echo "5. 退出"
     echo ""
     read -p "请输入操作编号： " option
 
@@ -19,9 +21,12 @@ while true; do
             ;;
         2)  wget https://raw.githubusercontent.com/Hamster-Prime/ospf-clash/main/install-ospf-clash_tproxy.sh && chmod +x install-ospf-clash_tproxy.sh && ./install-ospf-clash_tproxy.sh
             ;;
-        3)  wget https://raw.githubusercontent.com/Hamster-Prime/ospf-clash/main/install-ospf.sh && chmod +x install-ospf.sh && ./install-ospf.sh
+        3)  wget https://raw.githubusercontent.com/Hamster-Prime/ospf-clash/main/install-easymosdns.sh && chmod +x install-easymosdns.sh && ./install-easymosdns.sh
             ;;
-        4)  echo "退出脚本"
+        4)  wget https://raw.githubusercontent.com/Hamster-Prime/ospf-clash/main/install-ospf.sh && chmod +x install-ospf.sh && ./install-ospf.sh
+            exit 0
+            ;;
+        5)  echo "退出脚本"
             exit 0
             ;;
         *)  echo "无效选项，请重新选择"
