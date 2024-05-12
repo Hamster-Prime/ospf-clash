f8a3d67c27755174e6cbdf223d9237c95695c9df
Hamster-Prime <60058434+Hamster-Prime@users.noreply.github.com>
2024-05-12 21:15:43 +0800

    Update install-ospf.sh

--- install-ospf.sh
+++ install-ospf.sh
@@ -7,8 +7,11 @@ apt install bird git make curl wget sudo -y
 # 检测eth0的IP
 ip_address=$(ip addr show eth0 | grep -oP 'inet \K[\d.]+')
 
-#配置bird
+#配置bird服务
 mv /etc/bird/bird.conf /etc/bird/bird.conf.orig
+mv /etc/bird/bird6.conf /etc/bird/bird6.conf.orig
+
+#IPv4
 tee /etc/bird/bird.conf <<EOF
 router id $ip_address;
 
@@ -30,6 +33,40 @@ protocol device {
 
 protocol static {
     include "routes4.conf";
+}
+
+protocol ospf {
+    export all;
+
+    area 0.0.0.0 {
+        interface "eth0" {
+        };
+    };
+}
+EOF
+
+#IPv6
+tee /etc/bird/bird6.conf <<EOF
+router id $ip_address;
+
+# The Kernel protocol is not a real routing protocol. Instead of communicating
+# with other routers in the network, it performs synchronization of BIRD's
+# routing tables with the OS kernel.
+protocol kernel {
+	scan time 60;
+	import none;
+#	export all;   # Actually insert routes into the kernel routing table
+}
+
+# The Device protocol is not a real routing protocol. It doesn't generate any
+# routes and it only serves as a module for getting information about network
+# interfaces from the kernel. 
+protocol device {
+	scan time 60;
+}
+
+protocol static {
+    include "routes6.conf";
 }
 
 protocol ospf {
